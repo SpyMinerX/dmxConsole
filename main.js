f8ea006024d3dd82059ecd1d3f81a0516e29597b
SpyMIner <vince051105@icloud.com>
2023-02-18 18:19:35 +0100

    messing up shit

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
 const { app, BrowserWindow, ipcMain} = require('electron')
 const path = require('path')
 const updater = require('electron-simple-updater');
-
-let loadingWindow;
+let window;
 
 updater.init({
   checkUpdateOnStart: true,
@@ -24,21 +23,20 @@ ipcMain.handle('checkForUpdates', () => { updater.checkForUpdates(); });
 ipcMain.handle('downloadUpdate', () => { updater.downloadUpdate(); });
 ipcMain.handle('quitAndInstall', () => { updater.quitAndInstall(); });
 ipcMain.handle('setOption', (_, opt, val) => { updater.setOptions(opt, val); });
+ipcMain.on('gotomenue', (event, arg) => {
+  if(arg=='main')
+  {
+    window.loadFile('index.html');
+  }
+});
+  
 
 app.on('ready', () => {
-  loadingWindow = new BrowserWindow({
-    width: 640,
-    height: 480,
-    autoHideMenuBar: true,
-    webPreferences: {
-      nodeIntegration: true,
-      contextIsolation: false,
-    },
-  });
-
-  loadingWindow.loadFile('loading.html');
+  window = new BrowserWindow({autoHideMenuBar: true, fullscreen: true, resizable: false, webPreferences: {nodeIntegration: true, contextIsolation: false, enableRemoteModule: true}});
+  window.loadFile('loading.html');
+  setTimeout(() => window.loadFile('login.html'), 3000);
 });
 
 function ipcSend(event, ...args) {
-  loadingWindow?.webContents.send('updater-event', event, ...args);
+  window?.webContents.send('updater-event', event, ...args);
 }
